e3d9c5a7dcda07d17483e89c0c49c9d050948725
huynq <qhuydtvt@gmail.com>
2017-07-16 16:49:10 +0700

    Added records count today

--- src/components/user_detail.js
+++ src/components/user_detail.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 export default function(props) {
-  const {displayName, avatar} = props.user;
+  const {displayName, avatar, recordCountToday} = props.user;
   return (
     <div className="user-detail">
       <img className="user-avatar" src={avatar} />
-      <h4 className="user-name">  { displayName } </h4>
+      <h4 className="user-name">{ displayName }</h4>
+      <div className="default-margin recods-today">
+          <div className="records-today-label">Records today: </div>
+          <strong>{recordCountToday}</strong>
+      </div>
+      <button className="btn btn-warning default-margin">Log out</button>
     </div>
   );
 }
--- src/reducers/reducer_user.js
+++ src/reducers/reducer_user.js
@@ -1,9 +1,11 @@
-import { FETCH_USER } from  '../actions';
+import { FETCH_USER, ADD_RECORD } from  '../actions';
 
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
       return action.payload.data;
+    case ADD_RECORD:
+      return {...state, 'recordCountToday': state.recordCountToday + 1 };
     default:
       return state;
   }
