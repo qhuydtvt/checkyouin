20e3aed25f6bfe50b5a791163e2b1d436b9532c9
AVAVT <itsavavt@gmail.com>
2017-08-20 05:28:12 +0700

    Mobile navbar

--- src/components/login.js
+++ src/components/login.js
@@ -17,15 +17,13 @@ class Login extends Component {
     const { meta: {touched, error} } = field;
     const className = `form-group row ${touched && error ? "has-danger": ""}`;
     return (
-      <div className="wrapper container">
-        <div className={className}>
-          <label className="col-sm-2 col-form-label">{field.label} </label>
-          <div className="col-sm-10">
-              <input className="form-control" type={field.type} {...field.input}/>
-          </div>
-          <div className="text-help">
-            {touched ? error : ""}
-          </div>
+      <div className={className}>
+        <label className="col-sm-2 col-form-label">{field.label} </label>
+        <div className="col-sm-10">
+            <input className="form-control" type={field.type} {...field.input}/>
+        </div>
+        <div className="text-help">
+          {touched ? error : ""}
         </div>
       </div>
     );
@@ -35,33 +33,35 @@ class Login extends Component {
     const {handleSubmit, loginReducer} = this.props;
     const messageClassName = loginReducer.result === 1 ? "form-text text-success" : "form-text text-danger";
     return (
-      <div className='row login-modal'>
-        <div className="col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-          <h1>Check You In</h1>
-          <hr />
-          <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="login-form" >
-            <Field
-              label="Username"
-              name="username"
-              type="text"
-              component={this.renderInputField}
-            />
-            <Field
-              label="Password"
-              name="password"
-              type="password"
-              component={this.renderInputField}
-            />
-            {loginReducer.message &&
-              <div className="form-group">
-                <div className={messageClassName}>{loginReducer.message}</div>
-              </div>
+      <div className="wrapper container">
+        <div className='row login-modal'>
+          <div className="col-md-8 ml-md-auto mr-md-auto col-lg-6 offset-lg-3">
+            <h1>Check You In</h1>
+            <hr />
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))} className="login-form" >
+              <Field
+                label="Username"
+                name="username"
+                type="text"
+                component={this.renderInputField}
+              />
+              <Field
+                label="Password"
+                name="password"
+                type="password"
+                component={this.renderInputField}
+              />
+              {loginReducer.message &&
+                <div className="form-group">
+                  <div className={messageClassName}>{loginReducer.message}</div>
+                </div>
+              }
+              <button type="submit" disabled={loginReducer.isLoading} className="btn btn-primary btn-block login-button">Login</button>
+            </form>
+            {loginReducer.isLoading &&
+              <ReactLoading className="mx-auto" type='bubbles' color="#444" />
             }
-            <button type="submit" disabled={loginReducer.isLoading} className="btn btn-primary btn-block login-button">Login</button>
-          </form>
-          {loginReducer.isLoading &&
-            <ReactLoading className="mx-auto" type='bubbles' color="#444" />
-          }
+          </div>
         </div>
       </div>
     );
